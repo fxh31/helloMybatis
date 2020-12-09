7402a4a69957dea08326e783072bc5cccda67bfa
fxh31 <1194815021@qq.com>
2020-12-09 16:58:32 +0800

    mybatis xml plan2-注解before after test

--- src/test/java/com/fxh/test/UserDaoTest.java
+++ src/test/java/com/fxh/test/UserDaoTest.java
@@ -17,33 +17,52 @@ import static org.junit.Assert.*;
 
 public class UserDaoTest {
 
+    SqlSession session;
+    UserDao userDao;
     @Before
     public void setUp() throws Exception {
+        // 加载配置文件
+        SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(Resources.getResourceAsReader("mybatis-config.xml"));
+        //  获取session
+        session = factory.openSession();
+        userDao = session.getMapper(UserDao.class);
     }
 
     @After
     public void tearDown() throws Exception {
+
+        //提交事务
+        session.commit();
+        //释放seesion
+        session.close();
     }
 
-    @Test
-    public void findAll() throws IOException {// 加载配置文件
-        SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(Resources.getResourceAsReader("mybatis-config.xml"));
-        //  获取session
-        SqlSession session = factory.openSession();
 
-        UserDao userDao = session.getMapper(UserDao.class);
+    @Test
+    public void findAll() throws IOException {
+
+
         List<User> list = userDao.findAll();
 
         for(User user:list){
             System.out.println(user);
         }
 
-        //提交事务
-        session.commit();
-        //释放seesion
-        session.close();
 
     }
 
+    @Test
+    public void testFindById(){
+        User u1 = userDao.findById(1);
+        System.out.println(u1);
+    }
 
+    @Test
+    public void testSave(){
+        User user =new User();
+        user.setName("test");
+        user.setPassword("111111");
+        boolean result = userDao.save(user);
+        System.out.println(result);
+    }
 }
